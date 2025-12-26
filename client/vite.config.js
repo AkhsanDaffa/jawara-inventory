21efb1d4b1e57ec9af17034760709f48727d470b
AkhsanDaffa <akhsandaffa01@gmail.com>
2025-12-26 10:44:37 +0700

    fix: allow public domain host in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Ini agar bisa diakses dari luar container (0.0.0.0)
+    port: 5173,
+    // TAMBAHKAN BAGIAN INI:
+    allowedHosts: [
+      'my-gudang.jawaracodecorp.cloud',
+      'www.my-gudang.jawaracodecorp.cloud',
+      'all' // Opsional: gunakan 'all' jika ingin mengizinkan semua domain (lebih praktis)
+    ] 
+  }
 })
